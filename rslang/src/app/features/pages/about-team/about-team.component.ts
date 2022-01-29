73e9ba168850b3c9f5b0eb881d4853b4306246d8
Vladimir Bessonov <bessmy@gmail.com>
2022-01-29 23:08:53 +0300

    refactor: update temporary button to check api

--- rslang/src/app/features/pages/about-team/about-team.component.ts
+++ rslang/src/app/features/pages/about-team/about-team.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/core/services/api.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-about-team',
@@ -8,7 +9,7 @@ import { ApiService } from 'src/app/core/services/api.service';
 })
 export class AboutTeamComponent {
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, public auth: AuthService) { }
 
   onClickGetWordsButton() {
     this.api.getWords(0, 0).subscribe((response) => {
@@ -17,7 +18,7 @@ export class AboutTeamComponent {
   }
 
   onClickGetWordButton() {
-    this.api.getWord('5e9f5ee35eb9e72bc21af4a0').subscribe((response) => {
+    this.api.getWord('5e9f5ee35eb9e72bc21af4af').subscribe((response) => {
       console.log('resp:', response);
     });
   }
@@ -29,37 +30,38 @@ export class AboutTeamComponent {
   }
 
   onClickGetUserButton() {
-    this.api.getUser('61f30c12eb87140016ebfd14', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjMwMDdkZWI4NzE0MDAxNmViZmQxMiIsImlhdCI6MTY0MzMxODEyOCwiZXhwIjoxNjQzMzMyNTI4fQ.6O2pKL8AdP6SSN7VIHpYgy0K2u4c0rdDfnPTMasnaiM').subscribe((response) => {
+    this.api.getUser(this.auth.userId, this.auth.token).subscribe((response) => {
       console.log('resp:', response);
     });
   }
 
-  onClickSignInButton() {
-    this.api.signIn({ email: 'sss@jd.ru', password: '87654321' }).subscribe((response) => {
-      console.log('resp:', response);
-    });
+  onClickLoginButton() {
+    this.auth.login({ email: 'sss@jd.ru', password: '87654321' });
+  }
+
+  onClickLogoutButton() {
+    this.auth.logout();
+
   }
 
   onClickUpdateUserButton() {
-    this.api.updateUser('61f30c12eb87140016ebfd14', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjMwMDdkZWI4NzE0MDAxNmViZmQxMiIsImlhdCI6MTY0MzMxNjQyNiwiZXhwIjoxNjQzMzMwODI2fQ.PR1ipcMOBIhZ6i0R65ec5YNP1j580USJhsM_bgKlGhA', { email: 'sss@jd.ru', password: '87654321' }).subscribe((response) => {
+    this.api.updateUser(this.auth.userId, this.auth.token, { email: 'sss@jd.ru', password: '87654321' }).subscribe((response) => {
       console.log('resp:', response);
     });
   }
 
   onClickDeleteUserButton() {
-    this.api.deleteUser('61f30c12eb87140016ebfd14', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjMwMDdkZWI4NzE0MDAxNmViZmQxMiIsImlhdCI6MTY0MzMxODEyOCwiZXhwIjoxNjQzMzMyNTI4fQ.6O2pKL8AdP6SSN7VIHpYgy0K2u4c0rdDfnPTMasnaiM').subscribe((response) => {
+    this.api.deleteUser(this.auth.userId, this.auth.token).subscribe((response) => {
       console.log('resp:', response);
     });
   }
 
   onClickGetNewTokenButton() {
-    this.api.getNewUserTokens('61f30c12eb87140016ebfd14', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjMwYzEyZWI4NzE0MDAxNmViZmQxNCIsInRva2VuSWQiOiI0NjJhNTQ4Ny1iYjRiLTQ3OWItYjYzYS1iNjNlOTFlNjhjNWYiLCJpYXQiOjE2NDMzMTgzMjcsImV4cCI6MTY0MzMzNDUyN30.1Hd-V4v_0y4h2aACl8Aq7lNWCDB852yWKixoDvaIc1Y').subscribe((response) => {
-      console.log('resp:', response);
-    });
+    this.auth.updateLogin();
   }
 
   onClickGetAllUserWordsButton() {
-    this.api.getAllUserWords('61f30c12eb87140016ebfd14', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjMwYzEyZWI4NzE0MDAxNmViZmQxNCIsImlhdCI6MTY0MzMxODM1NiwiZXhwIjoxNjQzMzMyNzU2fQ.bIuNCDtGtB4DEblKu2LX7oPWPooOM7-jtW0_7iYhnsw').subscribe((response) => {
+    this.api.getAllUserWords(this.auth.userId, this.auth.token).subscribe((response) => {
       console.log('resp:', response);
     });
   }
