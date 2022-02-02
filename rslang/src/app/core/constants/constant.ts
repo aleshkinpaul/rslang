06807b656bb6d96ffc7c5885fac5a963082754a3
rubiaqute <rubiaqute@gmail.com>
2022-02-02 11:37:15 +0300

    feat: fix reloading  problems
    rebase to interceptor

--- rslang/src/app/core/constants/constant.ts
+++ rslang/src/app/core/constants/constant.ts
@@ -5,9 +5,13 @@ export const ERROR_MESSAGE = {
   updateLogin: 'Токен отсутствует, истек или недействителен',
   unauthorized: 'Пользователь не авторизован',
 }
+
 export const LEVELS_IN_GAME = 6;
 export const PAGES_IN_LEVEL = 30;
 export const WORDS_IN_GAME = 10;
 export const WORDS_ON_PAGE = 20;
+export const WORDS_IN_GAME_CHOICE = [5,10,15,20, 25,30]
 export const OPTIONS_IN_AUDIOCHALLENGE = 5;
 export const REFRESH_TIME = 300000;
+
+
