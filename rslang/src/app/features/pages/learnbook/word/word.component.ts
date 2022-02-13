ad87da9fadc4be2bb81e1c1b718b1594453f6a0b
Aleshkin Paul <vinci.aleshkin@gmail.com>
2022-02-13 21:10:07 +0300

    feat: add word card buttons

--- rslang/src/app/features/pages/learnbook/word/word.component.ts
+++ rslang/src/app/features/pages/learnbook/word/word.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IWord } from 'src/app/shared/interfaces';
 import { BACKEND_PATH } from 'src/app/core/constants/constant';
+import { ApiService } from 'src/app/core/services/api.service';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-word',
@@ -9,15 +13,17 @@ import { BACKEND_PATH } from 'src/app/core/constants/constant';
 })
 export class WordComponent implements OnInit {
   @Input() wordData!: IWord;
-  @Output() onClickAudio: EventEmitter<string[]> = new EventEmitter<string[]>();
+  @Output() clickAudio: EventEmitter<string[]> = new EventEmitter<string[]>();
   
   wordCardContentText: string = '';
   wordAudioSrcArr!: Array<string>;
   currentAudioInd: number = 0;
   isMeaningShow: boolean = false;
   isExampleShow: boolean = false;
+  isHard: boolean = false;
+  isKnown: boolean = false;
 
-  constructor() { }
+  constructor(private apiService: ApiService, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.wordAudioSrcArr = [this.wordData.audio, this.wordData.audioMeaning, this.wordData.audioExample];
@@ -28,8 +34,8 @@ export class WordComponent implements OnInit {
     return `${BACKEND_PATH}/${this.wordData.image}`;
   }
 
-  getAudio(link: string) {
-    return `${BACKEND_PATH}/${link}`;
+  getAudio(srcPath: string) {
+    return `${BACKEND_PATH}/${srcPath}`;
   }
 
   showMeaning() {
@@ -49,6 +55,40 @@ export class WordComponent implements OnInit {
   }
 
   playAudio() {
-    this.onClickAudio.emit(this.wordAudioSrcArr);
+    this.clickAudio.emit(this.wordAudioSrcArr);
+  }
+
+  addUserWord() {
+    this.apiService.getUserWordById(this.authService.userId, this.wordData.id)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            this.apiService.createUserWord(this.authService.userId, this.wordData.id, {
+              difficulty: this.isHard ? 'hard' : 'easy',
+              optional: {
+                tempParam: '',
+              }
+            })
+            .subscribe();
+          }
+          return throwError(() => error);
+        })
+      )
+      .subscribe(
+        (res) => this.apiService.updateUserWord(this.authService.userId, this.wordData.id, {
+          difficulty: this.isHard ? 'hard' : 'easy',
+          optional: {
+            tempParam: '',
+          }
+        })
+      )
+  }
+
+  checkAsHard() {
+
+  }
+
+  checkAsKnown() {
+
   }
 }
